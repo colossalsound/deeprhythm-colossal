5ae52497f03248eefaf57b0f2b33c71fa86e6254
huwcheston <hwc31@cam.ac.uk>
2025-07-24 18:02:25 +0100

    minor typing

--- deeprhythm_colossal/audio_proc/hcqm.py
+++ deeprhythm_colossal/audio_proc/hcqm.py
@@ -13,12 +13,16 @@ from deeprhythm_colossal.audio_proc.onset import onset_strength
 
 N_BINS = 240
 N_BANDS = 8
+N_FFT = 2048
+HOP_SIZE = 512
 
 
 def make_kernels(
-        len_audio: int = utils.SAMPLE_RATE * utils.CLIP_LENGTH,
-        sr=utils.SAMPLE_RATE,
-        device: str = utils.DEVICE
+        len_audio: int = utils.CLIP_SAMPLES,
+        sr: int = utils.SAMPLE_RATE,
+        device: str = utils.DEVICE,
+        n_fft: int = N_FFT,
+        hop_size: int = HOP_SIZE
 ):
     """
     Create the kernels for the STFT and CQT based on the input parameters.
@@ -27,34 +31,46 @@ def make_kernels(
         len_audio (int): The length of the audio signal in samples.
         sr (int): The sampling rate of the audio signal.
         device (str): The device to use for computation ('cuda' or 'cpu').
+        n_fft (int): The number of filters in the STFT.
+        hop_size (int): The hop size of the STFT.
 
     Returns:
         stft_spec (STFT): An object to compute the Short-Time Fourier Transform (STFT).
         band_filter (torch.Tensor): A filter matrix of shape (N_BANDS, n_fft_bins) to apply to the STFT.
         cqt_specs (list of CQT objects): A list of Constant-Q Transform (CQT) objects for different harmonics.
     """
-    n_fft = 2048
-    hop = 512
-    n_fft_bins = int(1+n_fft/2)
+    n_fft_bins = int(1 + n_fft / 2)
     band_filter = create_log_filter(n_fft_bins, N_BANDS, device=device)
-    stft_spec = feat.stft.STFT(sr=sr, n_fft=n_fft, hop_length=hop, output_format='Magnitude', verbose=False).to(device=device)
+    stft_spec = (
+        feat.stft.STFT(
+            sr=sr,
+            n_fft=n_fft,
+            hop_length=hop_size,
+            output_format='Magnitude',
+            verbose=False
+        ).to(device=device)
+    )
     cqt_specs = []
     for h in [1/2, 1, 2, 3, 4, 5]:
         # Convert from BPM to Hz
         fmin = (32.7 * h) / 60
         # TODO: check floor division here
-        sr_cqt = len_audio // (hop * 8)
+        sr_cqt = len_audio // (hop_size * 8)
         fmax = sr_cqt / 2
         num_octaves = np.log2(fmax / fmin)
         bins_per_octave = N_BINS / num_octaves
-        cqt_spec = feat.cqt.CQT(sr=sr_cqt,
-                                hop_length=len_audio//hop,
-                                n_bins=N_BINS,
-                                bins_per_octave=bins_per_octave,
-                                fmin=fmin,
-                                output_format='Magnitude',
-                                verbose=False,
-                                pad_mode='constant').to(device=device)
+        cqt_spec = (
+            feat.cqt.CQT(
+                sr=sr_cqt,
+                hop_length=len_audio // hop_size,
+                n_bins=N_BINS,
+                bins_per_octave=bins_per_octave,
+                fmin=fmin,
+                output_format='Magnitude',     # could be "Complex"?
+                verbose=False,
+                pad_mode='constant'
+            ).to(device=device)
+        )
         cqt_specs.append(cqt_spec)
     return stft_spec, band_filter, cqt_specs
 
@@ -78,9 +94,9 @@ def compute_hcqm(y: torch.Tensor, stft_spec, band_filter, cqt_specs: list) -> to
     """
     stft = stft_spec(y)
     stft_bands = apply_log_filter(stft, band_filter)
-    stft_bands_flat = stft_bands.reshape(stft.size(0)*stft_bands.size(1), stft_bands.size(2))
+    stft_bands_flat = stft_bands.reshape(stft.size(0) * stft_bands.size(1), stft_bands.size(2))
     osf_flat = onset_strength(y=stft_bands_flat)
-    hcqm = torch.zeros((stft.size(0)*N_BANDS, N_BINS, 6))
+    hcqm = torch.zeros((stft.size(0) * N_BANDS, N_BINS, 6))
     for h, spec in enumerate(cqt_specs):
         hcqm[:, :, h] = spec(osf_flat).mean(-1)
     hcqm = hcqm.reshape(stft_bands.size(0), N_BINS, N_BANDS, 6)
--- deeprhythm_colossal/model/frame_cnn.py
+++ deeprhythm_colossal/model/frame_cnn.py
@@ -7,9 +7,11 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+from deeprhythm_colossal import utils
+
 
 class DeepRhythmModel(nn.Module):
-    def __init__(self, num_classes=256):
+    def __init__(self, num_classes: int = len(utils.BPMS)):
         super(DeepRhythmModel, self).__init__()
         # input shape is (6, 240, 8)
         self.num_classes = num_classes
@@ -27,7 +29,6 @@ class DeepRhythmModel(nn.Module):
         self.elu = nn.ELU()
         self.dropout = nn.Dropout(0.5)
         self.fc2 = nn.Linear(256, num_classes)
-        self._initialize_weights()
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = F.relu(self.bn1(self.conv1(x)))
@@ -39,15 +40,3 @@ class DeepRhythmModel(nn.Module):
         x = self.dropout(self.elu(self.fc1(x)))
         x = self.fc2(x)
         return x
-
-    def _initialize_weights(self):
-        # TODO: this is probably not needed
-        for m in self.modules():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
-            elif isinstance(m, nn.BatchNorm2d):
-                nn.init.constant_(m.weight, 1)
-                nn.init.constant_(m.bias, 0)
-            elif isinstance(m, nn.Linear):
-                nn.init.xavier_normal_(m.weight)
-                nn.init.constant_(m.bias, 0)
