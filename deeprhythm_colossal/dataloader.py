4aacb0ce8dbf62ac69932ce90654d213779185f7
huwcheston <hwc31@cam.ac.uk>
2025-07-24 12:30:41 +0100

    moving dataloader

--- deeprhythm_colossal/dataloader.py
+++ deeprhythm_colossal/dataloader.py
@@ -1,7 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Data loading classes for both full songs and individual clips"""
+
 import h5py
 import torch
 from torch.utils.data import Dataset
+
 from deeprhythm_colossal.utils import bpm_to_class
+
+
+def song_collate(batch):
+    # Each element in `batch` is a tuple (song_clips, global_bpm)
+    # Where song_clips is a tensor of shape [num_clips, 240, 8, 6]
+    inputs = [item[0] for item in batch]
+    labels = torch.tensor([item[1] for item in batch])
+    return inputs, labels
+
+
 class ClipDataset(Dataset):
     def __init__(self, hdf5_file, group, use_float=False):
         """
@@ -41,3 +57,36 @@ class ClipDataset(Dataset):
             return hcqm_tensor, bpm
         label_class_index = bpm_to_class(int(bpm))  # Convert BPM to class index
         return hcqm_tensor, label_class_index
+
+
+class SongDataset(Dataset):
+    def __init__(self, hdf5_path, group):
+        """
+        Args:
+            hdf5_path (str): Path to the HDF5 file.
+            group (str): Group in HDF5 file ('train', 'test', 'validate').
+        """
+        super(SongDataset, self).__init__()
+        self.hdf5_path = hdf5_path
+        self.group = group
+        self.file = h5py.File(hdf5_path, 'r')
+        self.group_file = self.file[group]
+        self.keys = []
+        for key in self.group_file.keys():
+            if self.group_file[key].attrs['source'] == 'fma':
+                continue
+            else:
+                self.keys.append(key)
+
+    def __len__(self):
+        return len(self.keys)
+
+    def __getitem__(self, idx):
+        song_key = self.keys[idx]
+        song_data = self.group_file[song_key]
+        hcqm = torch.tensor(song_data['hcqm'][:])
+        bpm_class = bpm_to_class(int(float(song_data.attrs['bpm'])))
+        return hcqm, bpm_class
+
+    def close(self):
+        self.file.close()
